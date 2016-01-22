d7a4611c392137f72185b7a848fcfcef1a80c38b
Philip Shamsiev <ph.shamsiev@mail.ru>
2016-01-22 15:25:01 +0300

    module1-task2
    in check.js changed to Array.isArray()

--- js/check.js
+++ js/check.js
@@ -8,14 +8,14 @@ function getMessage(a, b) {
   else if (typeof a === "number") {
     return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " аттрибутов";
   }
-  else if (typeof a === "object" && typeof b === "object") {
+  else if (Array.isArray(a) && Array.isArray(b)) {
     var square = 0;
     for (var i = 0; i < a.length; i++) {
       square = square + (a[i] * b[i]);
     }
     return "Общая площадь артефактов сжатия: " + square + " пикселей";
   }
-  else if (typeof a === "object") {
+  else if (Array.isArray(a)) {
     var sum = 0;
     for (var i = 0; i < a.length; i++) {
       sum = sum + a[i];
